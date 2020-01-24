254abb6167fd89427af0c48bf616430ddbd6e491
lofidewanto <lofidewanto@gmail.com>
2020-01-24 18:59:53 +0100

    initial import

---
+++ src/test/java/com/lofi/example/SpringbootDddAspectjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lofi.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDddAspectjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

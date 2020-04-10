04e2d665e71052a4e3e67a99e88c2118582e4b2d
lofidewanto <lofidewanto@gmail.com>
2020-04-10 22:10:31 +0200

    update

--- src/main/java/de/crowdcode/bitemporal/example/SpringbootDddAspectjApplication.java
+++ src/main/java/de/crowdcode/bitemporal/example/SpringbootDddAspectjApplication.java
@@ -2,10 +2,8 @@ package de.crowdcode.bitemporal.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableLoadTimeWeaving;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 
-@EnableLoadTimeWeaving
 @EnableSpringConfigured
 @SpringBootApplication
 public class SpringbootDddAspectjApplication {

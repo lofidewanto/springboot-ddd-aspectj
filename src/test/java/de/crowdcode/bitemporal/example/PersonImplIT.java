b7a3fe7da40e25206eb4fab3c7e4dc9a18c98c57
lofidewanto <lofidewanto@gmail.com>
2020-04-15 23:40:28 +0200

    update test

--- src/test/java/de/crowdcode/bitemporal/example/PersonImplIT.java
+++ src/test/java/de/crowdcode/bitemporal/example/PersonImplIT.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 
 @SpringBootTest
+@EnableSpringConfigured
 class PersonImplIT {
 
 	private final static Logger logger = LoggerFactory.getLogger(PersonImplIT.class);
